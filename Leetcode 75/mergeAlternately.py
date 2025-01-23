741a52dd274146ecda9d963af8aecf225e9911e0
Anuj Attri <anujattri01@gmail.com>
2025-01-23 12:04:20 +0530

    added mergeAlternatively.py

---
+++ Leetcode 75/mergeAlternately.py
@@ -0,0 +1,29 @@
+'''
+1768. Merge Strings Alternately
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. 
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+'''
+
+'''
+Gist:
+Nested for-loop can be used, but a while-loop proves to be more scalable.
+'''
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = []
+        i, j = 0, 0
+
+        while i < len(word1) and j < len(word2):
+            merged.append(word1[i])
+            merged.append(word2[j])
+            i += 1
+            j += 1
+
+        if i < len(word1):
+            merged.append(word1[i:])
+        if j < len(word2):
+            merged.append(word2[j:])
+
+        return "".join(merged)
