d57e9b03f1f37d929e732262f328bb65f110f688
Anuj Attri <anujattri01@gmail.com>
2025-02-01 12:37:47 -0500

    solved findMaxAverage

--- Leetcode 75/findMaxAverage.py
+++ Leetcode 75/findMaxAverage.py
@@ -9,3 +9,14 @@ Any answer with a calculation error less than 10-5 will be accepted.
 '''
 
 '''
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        max_sum = window_sum = sum(nums[:k])
+        N = len(nums)
+
+        for i in range(k, N):
+            window_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum/k
