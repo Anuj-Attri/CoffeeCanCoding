5774d11e7476e615751d763346ca30354a7bb112
Anuj Attri <anujattri01@gmail.com>
2025-02-02 14:43:21 -0500

    solved increasingTriplets

--- Leetcode 75/increasingTriplet.py
+++ Leetcode 75/increasingTriplet.py
@@ -3,3 +3,26 @@ Given an integer array nums,
 return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. 
 If no such indices exists, return false.
 '''
+
+'''
+Gist:
+
+'''
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first_num = float('inf')
+        second_num = float('inf')
+
+        for i in range(len(nums)):
+
+            if first_num > nums[i]:
+                first_num = nums[i]
+
+            elif first_num < nums[i] and nums[i] < second_num:
+                second_num = nums[i]
+
+            elif second_num < nums[i]:
+                return True
+       
+        return False
