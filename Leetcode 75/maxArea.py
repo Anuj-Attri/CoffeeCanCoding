b432b5cdd9c96ad3754b8e70222e5f5a7823491e
Anuj Attri <anujattri01@gmail.com>
2025-02-03 09:30:37 -0500

    added gist of solution

--- Leetcode 75/maxArea.py
+++ Leetcode 75/maxArea.py
@@ -10,7 +10,19 @@ Notice that you may not slant the container.
 
 '''
 Gist:
+Using two-pointers, we can calculate and compare the area contained between two lines in linear time.
+Increment/decrement the pointers towards the next shorter height, so as to maximize the area contained between the two lines.
+The max between current and previous area is stored in 'area' and returned at the end of the loop.
+The standard approach, while linear time, performs many redundant calculation that consume CPU cycles, taking more time.
+To reduce the time taken, we eliminate heights that do not increase the area using a 'while-loop'.
+For example, "while left < right and height[left] <= min_height" compares if the next height is less than the previous height of the left pointer,
+and increments the left pointer till we find a height higher than the previous height.
+This step logically eliminates smaller areas without explicitlly calculating them, reducing the computation time to under 10% of original*.
 
+Time: O(N)
+Space: O(1)
+
+*As checked by leetcode test cases, solutions without eliminating redundant cycles takes over 100ms whereas this solution takes less than 10ms.
 '''
 
 class Solution:
