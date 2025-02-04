2dc240a95d67a94a50f56a4ad4639d1753d0e7ec
Anuj Attri <anujattri01@gmail.com>
2025-02-04 14:26:11 -0500

    solved maxOperationHashmap.py

--- Leetcode 75/maxOperationHashmap.py
+++ Leetcode 75/maxOperationHashmap.py
@@ -5,3 +5,21 @@ You are given an integer array nums and an integer k.
 In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
 Return the maximum number of operations you can perform on the array.
 '''
+
+from collections import Counter
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count = Counter(nums)
+        ops = 0
+
+        for i in nums:
+            if count[i] > 0 and count[k - i] > 0:
+                if i == k - i and count[i] < 2:
+                    continue
+
+                count[i] -= 1
+                count[k - i] -= 1
+                ops += 1
+
+        return ops
