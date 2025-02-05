ab2ddafd0b4b3ccd2389b1d9773e83945f0f3c9f
Anuj Attri <anujattri01@gmail.com>
2025-02-05 10:19:04 -0500

    solved maxVowels.py

--- Leetcode 75/maxVowels.py
+++ Leetcode 75/maxVowels.py
@@ -8,3 +8,23 @@ Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
 '''
 Gist:
 '''
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {"a", "e", "i", "o", "u"}
+        max_vowels = 0
+        current_vowels = 0
+
+        for i in range(k):
+            if s[i] in vowels:
+                current_vowels += 1
+        max_vowels = current_vowels
+
+        for i in range(k, len(s)):
+            if s[i] in vowels:  
+                current_vowels += 1
+            if s[i - k] in vowels:  
+                current_vowels -= 1
+            max_vowels = max(max_vowels, current_vowels)  
+
+        return max_vowels
