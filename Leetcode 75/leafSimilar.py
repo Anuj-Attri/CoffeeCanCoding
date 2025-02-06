163c088d446c63efe3ae502f8b80e87a6708ea7a
Anuj Attri <anujattri01@gmail.com>
2025-02-06 11:54:41 -0500

    solved leafsimilar

--- Leetcode 75/leafSimilar.py
+++ Leetcode 75/leafSimilar.py
@@ -6,3 +6,30 @@ Two binary trees are considered leaf-similar if their leaf value sequence is the
 
 Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
 '''
+
+'''
+Gist:
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def getleaves(root):
+            leaves = []
+
+            def dfs(node):
+                if not node:
+                    return
+                if not node.left and not node.right:
+                    leaves.append(node.val)
+
+                dfs(node.left)
+                dfs(node.right)
+
+            dfs(root)
+            return leaves
+        return getleaves(root1) == getleaves(root2)
