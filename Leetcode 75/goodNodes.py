95832dffa6a158927f487a4c515e1ec8c78db072
Anuj Attri <anujattri01@gmail.com>
2025-02-07 11:39:29 -0500

    solved goodNodes

--- Leetcode 75/goodNodes.py
+++ Leetcode 75/goodNodes.py
@@ -6,5 +6,19 @@ Return the number of good nodes in the binary tree.
 '''
 
 '''
+Gist:
 
 '''
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+
+            count = 1 if node.val >= max_so_far else 0
+            new_max = max(node.val, max_so_far)
+
+            return dfs(node.left, new_max) + dfs(node.right, new_max) + count
+
+        return dfs(root, root.val)
