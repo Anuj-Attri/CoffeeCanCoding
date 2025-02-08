8e05402e58a45b233b75972ea8b06bf2104dddfd
Anuj Attri <anujattri01@gmail.com>
2025-02-08 11:17:37 -0500

    added scalability

--- C++20/longestOnes.cpp
+++ C++20/longestOnes.cpp
@@ -1,6 +1,9 @@
 /*
 1004. Max Consecutive Ones III
 Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Time: O(N)
+Space: O(1)
 */
 
 #include <iostream>
