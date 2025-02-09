0f3e271134fb2370ca6f7a780ee688d4d111d734
Anuj Attri <anujattri01@gmail.com>
2025-02-09 11:12:07 -0500

    solved tribonacci

--- Leetcode 75/tribonacci.py
+++ Leetcode 75/tribonacci.py
@@ -6,3 +6,21 @@ T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
 Given n, return the value of Tn.
 '''
+
+'''
+Gist:
+
+'''
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n <= 0:
+            return 0
+
+        if n == 1 or n == 2: 
+            return 1
+
+        Tn, Tn1, Tn2 = 0, 1, 1  
+        for _ in range(3, n + 1):
+            Tn, Tn1, Tn2 = Tn1, Tn2, Tn + Tn1 + Tn2
+
+        return Tn2
