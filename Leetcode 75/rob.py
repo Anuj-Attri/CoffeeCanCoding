0bd7c42a00835a373c17d7d08a04f5bb08336572
Anuj Attri <anujattri01@gmail.com>
2025-02-16 16:32:33 -0500

    solved rob.py

--- Leetcode 75/rob.py
+++ Leetcode 75/rob.py
@@ -12,3 +12,12 @@ return the maximum amount of money you can rob tonight without alerting the poli
 Gist: 
 
 '''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev2, prev1 = 0, 0  
+
+        for num in nums:
+            new_rob = max(prev1, prev2 + num)  
+            prev2, prev1 = prev1, new_rob  
+
+        return prev1
