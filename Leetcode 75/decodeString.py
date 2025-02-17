17101f04951f4413dac602f5c75f7416c0e8b017
Anuj Attri <anujattri01@gmail.com>
2025-02-17 15:56:40 -0500

    solved decodeString

--- Leetcode 75/decodeString.py
+++ Leetcode 75/decodeString.py
@@ -13,3 +13,26 @@ The test cases are generated so that the length of the output will never exceed 
 '''
 
 '''
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        current_string = ""
+        num = 0
+
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+            
+            elif char == '[':
+                stack.append((current_string, num))
+                current_string = ""
+                num = 0
+            
+            elif char == ']':
+                last_string, repeat_times = stack.pop()
+                current_string = last_string + repeat_times * current_string
+            
+            else:
+                current_string += char  
+
+        return current_string
