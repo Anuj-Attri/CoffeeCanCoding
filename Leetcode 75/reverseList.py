e1146a0fdf21dac09d31319c8774319e8ae4aade
Anuj Attri <anujattri01@gmail.com>
2025-02-20 12:43:19 -0500

    added gist of solution

--- Leetcode 75/reverseList.py
+++ Leetcode 75/reverseList.py
@@ -2,7 +2,15 @@
 206. Reverse Linked List
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 '''
+'''
+Gist:
+Use two pointers (prev and curr) to reverse links one node at a time.
+Modify each node's next pointer to point to the previous node.
+Continue until all nodes are reversed.
 
+Time: O(N)
+Space: O(1)
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
